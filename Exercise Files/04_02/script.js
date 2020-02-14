0d6a0b59e6d3aa5cbf250ee283ca14506b2d09ba
avinash273 <avinash.shanker@mavs.uta.edu>
2020-02-14 04:06:53 -0600

    Functions

---
+++ Exercise Files/04_02/script.js
@@ -0,0 +1,11 @@
+function findBiggestFraction() {
+  console.log("The fucntion is  running!");
+  console.log("Which fraction is bigger");
+  a>b ? console.log("a: ",a) : console.log("b: ",b);
+
+}
+
+var a =3/4;
+var b = 50/7;
+
+findBiggestFraction();
